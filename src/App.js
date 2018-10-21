427c4e1757ca0d1b067fd14c81b54434c8425c53
leduyKim <LduyKim@gmail.com>
2018-10-21 17:45:40 +0700

    finished chapter 5: styling react components and element

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from  './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -44,15 +44,9 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
 
     let persons = null;
+    let btnClasses = '';
 
     if(this.state.showsPerson) {
       persons = (
@@ -66,21 +60,30 @@ class App extends Component {
                     changed = { (event) => this.nameChangeHandler(event, id) }/>
           })}
         </div>
-      )
+      );
+      btnClasses = classes.showPerson;
+    }
+
+    let assignedClasses = [];
+    if(this.state.persons.length <=2) {
+      assignedClasses.push(classes.red);
+    }
+
+    if(this.state.persons.length <=1) {
+      assignedClasses.push(classes.bold)
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!!!</p>
-        <button 
-          style = {style} 
-          onClick = {this.tooglePersonHandler} 
-          >Toogle Person</button>
-        { persons }
-      </div>
+        <div className={classes.App}>
+          <h1>Hi, I'm a React App</h1>
+          <p className = {assignedClasses.join(' ')} >This is really working!!!</p>
+          <button 
+            className = {btnClasses} 
+            onClick = {this.tooglePersonHandler} 
+            >Toogle Person</button>
+          { persons }
+        </div>
     );
   }
 }
-
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react'
-import './Person.css';
+import classes from  './Person.css';
 
 
-export default class Person extends Component {
+class Person extends Component {
     
 
     render() {
-        const style = {
-            backgroundColor: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer'
-        }
-
         const {name, age, click, changed} = this.props;
         return (
-            <div className="Person">
+            <div className={classes.Person}>
                 <p>I'm {name} and I am {age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange = {changed} placeholder = {name} />
-                <button onClick = {click} style={style} >Delete Element</button>
+                <button onClick = {click}>Delete Element</button>
             </div>
         )
     }
 }
+
+export default Person;
